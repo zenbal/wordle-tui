5889d6cb0d50c66b6340c5bab1ee17c21f551634
zenbal <helgebalzen@icloud.com>
2024-04-22 18:38:12 +0200

    test: trie deleteWord

--- wordle_test.go
+++ wordle_test.go
@@ -4,6 +4,13 @@ import (
 	"testing"
 )
 
+func NewTestTrie() *Trie {
+    trie := NewTrie()
+    trie.insertWord("hello")
+    trie.insertWord("world")
+    return &trie
+}
+
 func TestInsertWord(t *testing.T) {
 	trie := NewTrie()
 	trie.insertWord("hello")
@@ -23,4 +30,17 @@ func TestInsertTwoWords(t *testing.T) {
 	if trie.head.children[22].children[14].children[17].children[11].children[3].isWord != true {
 		t.Errorf("Test failed: Word 'world' not inserted correctly")
 	}
+}
+
+func TestDeleteWord(t *testing.T) {
+    trie := NewTestTrie()
+    trie.deleteWord("hello")
+    if trie.head.children[7] != nil {
+        t.Errorf("Test failed: Word 'hello' not deleted correctly")
+    }
+
+    trie.deleteWord("world")
+    if trie.head.children[22] != nil {
+        t.Errorf("Test failed: Word 'world' not deleted correctly")
+    }
 }
