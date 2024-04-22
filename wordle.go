d847877c93d1ad817875716218cece71f341a5b8
zenbal <helgebalzen@icloud.com>
2024-04-22 18:38:40 +0200

    trie: deleteWord

--- wordle.go
+++ wordle.go
@@ -9,6 +9,7 @@ func alphabet_idx(char byte) int {
 type Node struct {
 	value    byte
 	children []*Node
+	parent   *Node
 	isWord   bool
 }
 
@@ -17,6 +18,22 @@ func NewNode(value byte) *Node {
 		value:    value,
 		children: make([]*Node, 26),
 	}
+}
+
+func (n *Node) siblings() []*Node {
+    node_idx := alphabet_idx(n.value)
+    siblings := append(n.parent.children[:node_idx], n.parent.children[node_idx + 1:]...)
+    return siblings
+}
+
+func (n *Node) anySiblings() bool {
+    siblings := n.siblings()
+    for _, sibling := range siblings {
+        if sibling != nil {
+            return true
+        }
+    }
+    return false
 }
 
 type Trie struct {
@@ -38,9 +55,33 @@ func (t *Trie) insertWord(word string) {
 		var new_node *Node = nil
 		if next := curr.children[char_idx]; next == nil {
 			new_node = NewNode(word[i])
+            new_node.parent = curr 
 			curr.children[char_idx] = new_node
 		}
 		curr = new_node
 	}
 	curr.isWord = true
 }
+
+func (t *Trie) deleteWord(word string) {
+	curr := t.head
+	for i := 0; i < len(word); i++ {
+		char := word[i]
+		char_idx := alphabet_idx(char)
+		if next := curr.children[char_idx]; next != nil {
+			curr = next
+		} else {
+			return
+		}
+	}
+	curr_idx := alphabet_idx(curr.value)
+    for i := 0; i < len(word); i++ {
+       if curr.anySiblings() {
+           curr.parent.children[curr_idx] = nil
+           return
+       } 
+       curr = curr.parent
+       curr_idx = alphabet_idx(curr.value)
+    }
+}
+
